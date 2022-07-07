9a7eaf5e28ca9a761915f6cc94e9282daf388f39
Aliaksandr Kot <alven81.ak@gmail.com>
2022-07-07 17:41:21 +0300

    reinit

--- native/components/Todo.jsx
+++ native/components/Todo.jsx
@@ -1,20 +1,60 @@
 import { Button, Text, View } from "react-native";
-import AsyncStorage from "@react-native-community/async-storage";
+import AsyncStorage from "@react-native-async-storage/async-storage";
+import { useEffect, useState } from "react";
 
 const Todo = () => {
+
+    const [input, setInput] = useState();
+    const todoArray = [];
+
+    useEffect(() => {
+        if (readTodoLength()) {
+            //for (i = 0; i < readTodoLength(); i++) {
+            //    todoArray[i] = readData(i)
+            //}
+        } else {
+            alert("no todo data")
+        }
+    }) 
+
+    const readTodoLength = async () => {
+		try {
+			const value = await AsyncStorage.getItem("0");
+			if (value !== null) {
+				setInput(value);
+			}
+		} catch (e) {
+			alert("Failed to fetch the input from storage");
+		}
+	};
+
 	const saveData = async (age) => {
 		try {
-			await AsyncStorage.setItem(STORAGE_KEY, age);
+			await AsyncStorage.setItem("test", age);
 			alert("Data successfully saved");
 		} catch (e) {
 			alert("Failed to save the data to the storage");
 		}
 	};
 
+	const readData = async () => {
+		try {
+			const value = await AsyncStorage.getItem("test");
+			if (value !== null) {
+				setInput(value);
+			}
+		} catch (e) {
+			alert("Failed to fetch the input from storage");
+		}
+	};
+
 	return (
 		<>
-			<Text>Text</Text>
-			<Button title="test" onPress={() => saveData(25)} />
+			<Text>{input}</Text>
+			<Button title="save" onPress={() => saveData(20)} />
+            <Button title="read" onPress={() => readData()} />
+            <Button title="read" onPress={setInput(todoArray)} />
+            
 		</>
 	);
 };
